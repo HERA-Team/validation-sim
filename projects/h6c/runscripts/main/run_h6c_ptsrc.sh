0fb0dc7a5f6ad4c79d734600ddfe68aafa36f416
Steven Murray <murray.steveng@gmail.com>
2026-03-30 15:43:35 +0200

    fix: ensure scripts run vsim directly

--- projects/h6c/runscripts/main/run_h6c_ptsrc.sh
+++ projects/h6c/runscripts/main/run_h6c_ptsrc.sh
@@ -1,12 +1,12 @@
-./vsim.py runsim \
+vsim runsim \
   --log-level INFO --skip-existing --simulator fftvis64 \
   --redundant --layout FULL \
   --slurm-override time '03:00:00' --slurm-override partition RM  \
   --sky-model ptsrc1024 --spline-interp-order 1 \
   --prefix final \
-  --n-time-chunks 1 --do-time-chunks 0 --channels 900~1100
+  --n-time-chunks 1 --do-time-chunks 0 --channels 0~1100
 
-./vsim.py runsim \
+vsim runsim \
   --log-level INFO --skip-existing --simulator fftvis32 \
   --redundant --layout FULL \
   --slurm-override time '10:00:00' --slurm-override partition RM  \
