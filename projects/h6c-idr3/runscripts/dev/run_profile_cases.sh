47bd05902e07dbc50726f826390ef10961b0fa69
Steven Murray <murray.steveng@gmail.com>
2026-04-10 22:36:04 +0200

    deps: update deps for h6c-idr3

--- projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
+++ projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
@@ -2,8 +2,8 @@
 common="--log-level INFO --skip-existing --gpu --do-time-chunks 0 --channels 100 --profile --slurm-override time '01:00:00' --redundant"
 
 # These two simply check that 120 time chunks is sufficient to avoid excessive overhead.
-vsim runsim ${common} --n-time-chunks 120 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam --redundant
-vsim runsim ${common} --n-time-chunks 60 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam --redundant
+vsim runsim ${common} --n-time-chunks 120 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam
+vsim runsim ${common} --n-time-chunks 60 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam
 
 # Then, we'll want some that test fftvis vs matvis,
 
