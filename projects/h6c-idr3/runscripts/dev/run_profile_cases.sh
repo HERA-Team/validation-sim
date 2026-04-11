9b4d0db32ea00ff47a562b15fed0814dd621d278
Steven Murray <steven.g.murray@asu.edu>
2026-04-11 02:21:03 -0400

    add hera_sim dep

--- projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
+++ projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
@@ -3,7 +3,7 @@ common="--log-level INFO --skip-existing --gpu --do-time-chunks 0 --channels 100
 
 # These two simply check that 120 time chunks is sufficient to avoid excessive overhead.
 uv run vsim runsim ${common} --n-time-chunks 120 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam
-uv runvsim runsim ${common} --n-time-chunks 60 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam
+uv run vsim runsim ${common} --n-time-chunks 60 --sky-model ptsrc1024 --layout FULL --simulator matvis --prefix h6c-idr3-profiling-1beam
 
 # Then, we'll want some that test fftvis vs matvis,
 
