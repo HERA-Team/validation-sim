f20ad110db218611274f136cfcabd1749dad30c8
Steven Murray <murray.steveng@gmail.com>
2026-04-30 13:42:50 +0200

    deps: depend on hera-sim branch temporarily

--- projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
+++ projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
@@ -10,7 +10,7 @@ common="--log-level INFO --skip-existing --do-time-chunks 0 --channels 100 --pro
 # uv run vsim --uv runsim ${common} --gpu --n-time-chunks 60 --simulator matvis --n-unique-beams 350 --prefix h6c-idr3-profiling-matvis
 
 # Then, we'll want some that test fftvis vs matvis,
-uv run vsim --uv runsim ${common} --cpu --n-time-chunks 120 --simulator fftvis --n-unique-beams 1 --prefix h6c-idr3-profiling-fftvis-cpu
+uv run vsim --uv runsim ${common} --cpu --n-time-chunks 120 --simulator fftvis --n-unique-beams 1 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 6
 # uv run vsim --uv runsim ${common} --cpu --n-time-chunks 120 --simulator fftvis --n-unique-beams 350 --prefix h6c-idr3-profiling-fftvis-cpu
 
 # And some that test polarized sky vs not
