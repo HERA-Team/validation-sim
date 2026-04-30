722c3e650831b22838e4ddc6a5aba5e96ace182c
Steven Murray <murray.steveng@gmail.com>
2026-04-30 16:32:35 +0200

    update to use 135 time chunks, so exactly 128 times (4 per thread)

--- projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
+++ projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
@@ -1,16 +1,16 @@
 
 common="--log-level INFO --skip-existing --do-time-chunks 0 --channels 100 --profile --slurm-override time '01:00:00' --not-redundant  --sky-model ptsrc1024 --layout FULL"
 
-# These two simply check that 120 time chunks is sufficient to avoid excessive overhead.
-uv run vsim --uv runsim ${common} --gpu --n-time-chunks 120 --simulator matvis  --prefix h6c-idr3-profiling-matvis
-# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 60 --simulator matvis --prefix h6c-idr3-profiling-matvis
+# These two simply check that 135 time chunks is sufficient to avoid excessive overhead (corresponds to 128 times in each chunk)
+# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 135 --simulator matvis  --prefix h6c-idr3-profiling-matvis
+# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 270 --simulator matvis --prefix h6c-idr3-profiling-matvis
 
 # And some that tests multi-beam vs single-beam
-# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 120 --simulator matvis --n-unique-beams 350 --prefix h6c-idr3-profiling-matvis
-# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 60 --simulator matvis --n-unique-beams 350 --prefix h6c-idr3-profiling-matvis
+# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 135 --simulator matvis --n-unique-beams 350 --prefix h6c-idr3-profiling-matvis
+# uv run vsim --uv runsim ${common} --gpu --n-time-chunks 270 --simulator matvis --n-unique-beams 350 --prefix h6c-idr3-profiling-matvis
 
 # Then, we'll want some that test fftvis vs matvis,
-# uv run vsim --uv runsim ${common} --cpu --n-time-chunks 120 --simulator fftvis --n-unique-beams 1 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 6
-# uv run vsim --uv runsim ${common} --cpu --n-time-chunks 120 --simulator fftvis --n-unique-beams 350 --prefix h6c-idr3-profiling-fftvis-cpu
+uv run vsim --uv runsim ${common} --cpu --n-time-chunks 135 --simulator fftvis32 --n-unique-beams 1 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 64
+uv run vsim --uv runsim ${common} --cpu --n-time-chunks 135 --simulator fftvis32 --n-unique-beams 350 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 64
 
 # And some that test polarized sky vs not
