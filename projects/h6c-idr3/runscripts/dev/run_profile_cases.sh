7af237655770ae89a3dc1bdf68e644c47ebf4941
Steven Murray <steven.g.murray@asu.edu>
2026-04-30 16:58:12 -0400

    add --no-compress to profiling

--- projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
+++ projects/h6c-idr3/runscripts/dev/run_profile_cases.sh
@@ -1,5 +1,5 @@
 
-common="--log-level INFO --skip-existing --do-time-chunks 0 --channels 100 --profile --slurm-override time '00:15:00' --not-redundant  --sky-model ptsrc1024 --layout FULL"
+common="--log-level INFO --skip-existing --do-time-chunks 0 --channels 100 --profile --slurm-override time '00:15:00' --not-redundant  --sky-model ptsrc1024 --layout FULL --no-compress"
 
 # These two simply check that 135 time chunks is sufficient to avoid excessive overhead (corresponds to 128 times in each chunk)
 # uv run vsim --uv runsim ${common} --gpu --n-time-chunks 135 --simulator matvis  --prefix h6c-idr3-profiling-matvis
@@ -10,7 +10,7 @@ common="--log-level INFO --skip-existing --do-time-chunks 0 --channels 100 --pro
 # uv run vsim --uv runsim ${common} --gpu --n-time-chunks 270 --simulator matvis --n-unique-beams 350 --prefix h6c-idr3-profiling-matvis
 
 # Then, we'll want some that test fftvis vs matvis,
-uv run vsim --uv runsim ${common} --cpu --n-time-chunks 135 --simulator fftvis32 --n-unique-beams 1 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 64
-uv run vsim --uv runsim ${common} --cpu --n-time-chunks 135 --simulator fftvis32 --n-unique-beams 350 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 64
+# uv run vsim --uv runsim ${common} --cpu --n-time-chunks 135 --simulator fftvis32 --n-unique-beams 1 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 64
+uv run vsim --uv runsim ${common} --cpu --n-time-chunks 135 --simulator fftvis32 --n-unique-beams 10 --prefix h6c-idr3-profiling-fftvis-cpu --slurm-override ntasks-per-node 64
 
 # And some that test polarized sky vs not
