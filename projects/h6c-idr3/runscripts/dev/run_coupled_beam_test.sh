e17c496c36990015bff32968869468918fe4ab84
Steven Murray <murray.steveng@gmail.com>
2026-06-25 22:32:13 +0200

    fix: update to uv run

--- projects/h6c-idr3/runscripts/dev/run_coupled_beam_test.sh
+++ projects/h6c-idr3/runscripts/dev/run_coupled_beam_test.sh
@@ -1,12 +1,12 @@
 
-common="--log-level DEBUG --skip-existing --profile --profile-timer-unit 1e-2 --simulator fftvis --force-remake-obsparams --not-redundant --layout HEX"
+common="--log-level DEBUG --skip-existing --profile --profile-timer-unit 1e-2 --simulator fftvis32-coupled --force-remake-obsparams --not-redundant --layout HEX --no-compress"
 
 # We do the two different spline interp orders with different channels or time chunks so they don't overwrite each other.
 
 # Diffuse long-time-axis tests. 3 hours for two channels to check FRF.
 dimensions=" --n-time-chunks 8640 --do-time-chunks 0 --channels 1 --n-unique-beams 42"
 
-./vsim.py runsim ${common} ${dimensions}  --prefix coupled --slurm-override time '03:00:00' --slurm-override ntasks 3 --sky-model gsm_nside1024 --spline-interp-order 3 --coupled
+uv run vsim --uv runsim ${common} --cpu ${dimensions}  --prefix coupled --slurm-override time '03:00:00' --slurm-override ntasks-per-node 128 --slurm-ocerride partition RM --sky-model gsm_nside1024 --spline-interp-order 3 --coupled
 
 
 
